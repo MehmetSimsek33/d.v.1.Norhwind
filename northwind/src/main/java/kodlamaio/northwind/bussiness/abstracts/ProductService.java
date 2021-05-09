e553f53a09a944c3af08509c50f161e245970319
muo <muo@DESKTOP-GMIJAD5>
2021-05-09 16:29:23 +0300

    d.v.1.Norhwind

---
+++ northwind/src/main/java/kodlamaio/northwind/bussiness/abstracts/ProductService.java
@@ -0,0 +1,12 @@
+package kodlamaio.northwind.bussiness.abstracts;
+
+import java.util.List;
+
+import kodlamaio.northwind.entities.concrete.Product;
+
+public interface ProductService 
+{
+	List<Product> getAll();
+	
+
+}
